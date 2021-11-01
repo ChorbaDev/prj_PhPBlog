6a7e847e93db489af35e63d92af67685b34ea237
Younes Ghoniem <60320772+Dhoulnoun@users.noreply.github.com>
2021-11-01 12:08:13 +0100

    update guiStyle toggleScript.js

--- vue/javascriptfiles/toggleScript.js
+++ vue/javascriptfiles/toggleScript.js
@@ -1,5 +1,5 @@
 function toggleMenu(classparam){
-    var x= document.querySelector(classparam)
+    let x = document.querySelector(classparam);
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
