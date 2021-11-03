e57961f04b9ab4b8e3c486caac1dc2ae55961290
Dhoulnoun <60320772+Dhoulnoun@users.noreply.github.com>
2021-11-03 11:42:06 +0100

    update header.php

--- vue/phpfiles/header.php
+++ vue/phpfiles/header.php
@@ -1,45 +1,33 @@
 <script type="text/javascript" src="../javascriptfiles/toggleScript.js"></script>
-   <header>
-       <div class="logo">
-           <h1 class="logo-txt"><i class="fas fa-book-reader"></i>The Blog</h1>
-       </div>
-       <i class="fa fa-bars menu-switch" onclick="toggleMenu('.nav')"></i>
-       <ul class="nav">
-           <li><a href="accueil.php">Accueil</a></li>
-           <li><a href="..\htmlfiles\apropos.html">A Propos</a></li>
-           <!--        <li><a href="#">S'inscrire</a></li>-->
-           <!--        <li><a href="#">Se Connecter</a></li>-->
-           <li>
-               <a href="#" onclick="toggleMenu('.nav ul')">
-                   <i class="fa fa-user"></i>
-                   <?php
-                   //session_start();
-                   if(isset($_SESSION['pseudo'])){
-                       echo $_SESSION['pseudo'];
+<?php
+session_start();
+?>
+<header>
+    <div class="logo">
+        <h1 class="logo-txt"><i class="fas fa-book-reader"></i>The Blog</h1>
+    </div>
+    <i class="fa fa-bars menu-switch" onclick="toggleMenu('.nav')"></i>
+    <ul class="nav">
+        <li><a href="accueil.php">Accueil</a></li>
+        <li><a href="..\htmlfiles\apropos.html">A Propos</a></li>
+        <?php if (isset($_SESSION['pseudo'])): ?>
+            <li>
+                <a href="#" onclick="toggleMenu('.nav ul')">
+                    <i class="fa fa-user"></i>
+                    <?php echo $_SESSION['pseudo'];
+                    ?>
 
-                   }
-                   else{
-                       echo "Utilisateur";
-                   }
-                   ?>
-                   <i class="fa fa-chevron-down"></i>
-               </a>
-               <?php
-               //session_start();
-               if(isset($_SESSION['pseudo'])){
-                   echo"
-                           <ul>
-                            <li><a href='#'>Tableau de Bord</a></li>
-                            <li><a href='deconnexion.php' class='deconnexion'>Se Déconnecter</a></li>
-                           </ul>";
-               }
-               else{
-                   echo"
-                           <ul>
-                            <li><a href='login.php' class='connexion'>Se Connecter</a></li>
-                           </ul>";
-               }
-               ?>
-           </li>
-       </ul>
-   </header>
+                    <i class="fa fa-chevron-down"></i>
+                </a>
+                <ul>
+                    <li><a href="#">Tableau de Bord</a></li>
+                    <li><a href="deconnexion.php" class="deconnexion">Se Déconnecter</a></li>
+                </ul>
+            </li>
+        <?php else: ?>
+                <li><a href="signup.php">S'inscrire</a></li>
+                <li><a href="login.php">Se Connecter</a></li>
+        <?php endif;?>
+
+    </ul>
+</header>
